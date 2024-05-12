2fad3e3af56d24e6784580e5476106e8c8338359
Saiprasad Toshatwad <77664251+dev-hack95@users.noreply.github.com>
2024-05-12 19:35:57 +0530

    Delete Basics directory

--- Basics/chapter-3/dt_5/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
-    v.insert(5, 7);
-    v.remove(1);
-    println!("{:?}", v);
-}
--- Basics/chapter-5/refrences11/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let mut y = 10;
-    let m1 = &mut y; 
-    //let m2 = &mut y; // error: can't borrow as mutable more than once
-    let z = y; // error: can't use `y` beacuse it was mutable borrowed
-    //println!("{}, {}, {}", m1, m2, z);
-}
