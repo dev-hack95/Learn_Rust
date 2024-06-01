33fce6ec53d8df50300e8874a98b569c8ad7065c
dev-hack95 <saiprasadtoshatwad@gmail.com>
2024-06-01 12:35:26 +0530

    pixel to point

--- Basics/chapter-1/mandelbrot/src/main.rs
+++ Basics/chapter-1/mandelbrot/src/main.rs
@@ -45,6 +45,20 @@ fn parse_pair<T: FromStr>(s: &str, seperator: char) -> Option<(T, T)> {
     }
 }
 
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        // for literal use single quotes
+        Some((re, im)) => Some(Complex{re, im}),
+        None => None
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex{re: 1.25, im: -0.0625}));
+    assert_eq!(parse_complex(",-123"), None);
+}
+
 #[test]
 fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("", ','), None);
@@ -53,6 +67,27 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
     assert_eq!(parse_pair("10x20", 'x'), Some((10, 20)));
 }
+
+/// Given the row and column of a pixel in output image,
+/// return the corresponding point on the complex plane.
+
+fn pixel_to_point(bounds: (usize, usize), pixel: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) -> Complex<f64> {
+    let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width  / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+        // Why subtraction here? pixel.1 increases as we go down,
+        // but the imaginary component increases as we go up.
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 200), (25, 175),
+                              Complex { re: -1.0, im:  1.0 },
+                              Complex { re:  1.0, im: -1.0 }),
+               Complex { re: -0.5, im: -0.75 });
+} 
 
 fn main() {
     //square_loop(0.5);
