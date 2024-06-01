0d7419e4cedf53cc058f845b3c97ec77bb24d93c
dev-hack95 <saiprasadtoshatwad@gmail.com>
2024-06-01 14:13:07 +0530

    mandelet complete

--- Basics/chapter-1/mandelbrot/src/main.rs
+++ Basics/chapter-1/mandelbrot/src/main.rs
@@ -1,10 +1,15 @@
-use::num::Complex;
-use::std::str::FromStr;
+use num::Complex;
+use std::env;
+use std::str::FromStr;
+use image::ColorType;
+use image::codecs::png::PngEncoder;
+use std::fs::File;
+
 /// The FromStr trait is used for converting a string into another type.
 
 #[warn(dead_code)]
 fn complex_square_add_loop(c: Complex<f64>) {
-    let mut z: Complex<f64> = Complex{re: 0.2, im: 0.3};
+    let mut z: Complex<f64> = Complex { re: 0.2, im: 0.3 };
     loop {
         z = z * z + c;
     }
@@ -12,7 +17,7 @@ fn complex_square_add_loop(c: Complex<f64>) {
 
 #[warn(dead_code)]
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
-    let mut z = Complex{re: 0.0, im: 0.0};
+    let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
             return Some(i);
@@ -22,21 +27,20 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     None
 }
 
-/// Parse the string `s` as a co-ordinate pair, like `"400x6000"` or `"1.0, 0.5"`
-/// Specifically `s` should have thr form <left><sep><right> are
-/// both strings that can be parsed by `T::from_str` `seperator` must be an ASCII character
-/// If `s` has the proper form, return `Some<(x, y)>`. If it doesn't parse
-/// correctly, return `None`
+/// Parse the string `s` as a coordinate pair, like `"400x600"` or `"1.0,0.5"`
+/// Specifically, `s` should have the form <left><sep><right>
+/// where both <left> and <right> are strings that can be parsed by `T::from_str`
+/// `separator` must be an ASCII character. If `s` has the proper form,
+/// return `Some<(x, y)>`. If it doesn't parse correctly, return `None`.
 
-
-/// `<T: FromStr>`: This is a generic type prameter. It indicates that the
-/// function works with any type `T` as long as `T` implements the `FromStr`
-/// The function return a tuple of two calues of type `T` i.e Option<(T, T)> 
-/// we can define parse pair as i32 i.e parse_pair::<i32> or parse_pair::<f64>
-fn parse_pair<T: FromStr>(s: &str, seperator: char) -> Option<(T, T)> {
-    match s.find(seperator) {
-        None => None, // If seperator not found then return None
-        Some(index) => { // If seperator found we proceed with parsing substring
+/// `<T: FromStr>`: This is a generic type parameter. It indicates that the
+/// function works with any type `T` as long as `T` implements the `FromStr` trait.
+/// The function returns a tuple of two values of type `T`, i.e., `Option<(T, T)>`.
+/// We can define parse_pair as `parse_pair::<i32>` or `parse_pair::<f64>`.
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None, // If separator not found then return None
+        Some(index) => { // If separator found we proceed with parsing substring
             match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
                 (Ok(l), Ok(r)) => Some((l, r)),
                 _ => None
@@ -47,15 +51,15 @@ fn parse_pair<T: FromStr>(s: &str, seperator: char) -> Option<(T, T)> {
 
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
     match parse_pair(s, ',') {
-        // for literal use single quotes
-        Some((re, im)) => Some(Complex{re, im}),
+        // For literals, use single quotes
+        Some((re, im)) => Some(Complex { re, im }),
         None => None
     }
 }
 
 #[test]
 fn test_parse_complex() {
-    assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex{re: 1.25, im: -0.0625}));
+    assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex { re: 1.25, im: -0.0625 }));
     assert_eq!(parse_complex(",-123"), None);
 }
 
@@ -68,29 +72,65 @@ fn test_parse_pair() {
     assert_eq!(parse_pair("10x20", 'x'), Some((10, 20)));
 }
 
-/// Given the row and column of a pixel in output image,
+/// Given the row and column of a pixel in the output image,
 /// return the corresponding point on the complex plane.
-
 fn pixel_to_point(bounds: (usize, usize), pixel: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) -> Complex<f64> {
+    // Set width and height of the pixel area
     let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
     Complex {
-        re: upper_left.re + pixel.0 as f64 * width  / bounds.0 as f64,
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
         im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
         // Why subtraction here? pixel.1 increases as we go down,
         // but the imaginary component increases as we go up.
     }
 }
 
+fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let encoder = PngEncoder::new(output);
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::L8);
+    Ok(())
+}
+
 #[test]
 fn test_pixel_to_point() {
-    assert_eq!(pixel_to_point((100, 200), (25, 175),
-                              Complex { re: -1.0, im:  1.0 },
-                              Complex { re:  1.0, im: -1.0 }),
-               Complex { re: -0.5, im: -0.75 });
-} 
+    assert_eq!(
+        pixel_to_point((100, 200), (25, 175), Complex { re: -1.0, im: 1.0 }, Complex { re: 1.0, im: -1.0 }),
+        Complex { re: -0.5, im: -0.75 }
+    );
+}
 
 fn main() {
-    //square_loop(0.5);
-    //complex_square_add_loop(Complex{re: 1.0, im: 2.0});
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1.0,0.20", args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
