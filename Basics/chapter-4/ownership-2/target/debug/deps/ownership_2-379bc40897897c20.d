9c403c6ef529e568bb2d6fcc4570f0823e7c6260
dev-hack95 <saiprasadtoshatwad@gmail.com>
2024-07-15 19:16:26 +0530

    new changes

--- Basics/chapter-4/ownership-2/target/debug/deps/ownership_2-379bc40897897c20.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/Rust/Learn_Rust/Basics/chapter-4/ownership-2/target/debug/deps/libownership_2-379bc40897897c20.rmeta: src/main.rs
-
-/home/ubuntu/Rust/Learn_Rust/Basics/chapter-4/ownership-2/target/debug/deps/ownership_2-379bc40897897c20.d: src/main.rs
-
-src/main.rs:
--- Basics/chapter-4/ownership-2/target/debug/deps/ownership_2-47dcc364589352f8.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/Rust/Learn_Rust/Basics/chapter-4/ownership-2/target/debug/deps/libownership_2-47dcc364589352f8.rmeta: src/main.rs
-
-/home/ubuntu/Rust/Learn_Rust/Basics/chapter-4/ownership-2/target/debug/deps/ownership_2-47dcc364589352f8.d: src/main.rs
-
-src/main.rs:
