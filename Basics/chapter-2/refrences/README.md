3ba71fc0c8486f3bd221bac24fe655152b855c24
dev-hack95 <saiprasadtoshatwad@gmail.com>
2024-07-15 20:09:18 +0530

    refrences readme update

--- Basics/chapter-2/refrences/README.md
+++ Basics/chapter-2/refrences/README.md
@@ -5,3 +5,10 @@
    but it have one condition that it can have 
    only one refrence to a particular varible
 
+* i.e
+```bash
+let mut s = String::from("Hello");
+let r1 = &mut s;
+let r2 = &mut s; // this line will throw error
+// in rust two pointers can be assigned to one value
+```
---
+++ Basics/chapter-2/refrences/target/debug/deps/refrences-6aca71f05a90e096.d
@@ -0,0 +1,9 @@
+/home/ubuntu/Rust/Learn_Rust/Basics/chapter-2/refrences/target/debug/deps/librefrences-6aca71f05a90e096.rmeta: src/main.rs Cargo.toml
+
+/home/ubuntu/Rust/Learn_Rust/Basics/chapter-2/refrences/target/debug/deps/refrences-6aca71f05a90e096.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
