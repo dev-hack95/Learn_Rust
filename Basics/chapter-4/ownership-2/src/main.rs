9c4a743ce4d1e2873f41b5fc61185750fb00c84f
dev-hack95 <saiprasadtoshatwad@gmail.com>
2024-07-15 19:53:48 +0530

    refrence varibles and their access level in the programe

--- Basics/chapter-4/ownership-2/src/main.rs
+++ Basics/chapter-4/ownership-2/src/main.rs
@@ -12,14 +12,50 @@ fn main() {
     // println!("{:?}", str1); -> this line of code will not work
     // beacuse str1 have lost the ownership of str1
 
-    take_ownership(str2);
-    //println!("{}", str2);
+    let str3 = return_ownership(str2);
+    take_ownership(str3);
+    //println!("{}", str3);
+
+    indexed_ownership_acces();
+    refrence_ownership();
 }
 
+/// The function uses allocated varible and droopped as soon as the programme is finished
 fn take_ownership(input_str: String) {
-    println!("{}", input_str);
+    println!("{}", input_str); // input_str is used here
+                               // deallocated a varibele  e
 }
 
-fn returnownership(input_str: String) {
-    return input_str;
+fn return_ownership(input_str: String) -> String {
+    println!("{}", input_str);
+    input_str
+}
+
+fn indexed_ownership_acces() {
+    let mut v = Vec::new();
+    for i in 1..5 {
+        v.push(i.to_string());
+    }
+    println!("{:?}", v);
+
+    // let third = v[2]; -> You can't assigned v[2] to a varible name third
+    // you can access a varible via indexing but can't assign the varible to
+    // the other varible
+}
+
+fn refrence_ownership() {
+    // If you want to use a varible without taking ownership of that
+    // we use refrences there to manage these condtions
+
+    let s1 = String::from("Hello WOrld");
+    let (s2, length) = calculate_length_of_string(&s1);
+    println!("String: {} and Length: {}", s2, length);
+    println!("Can i access s1: {}", s1);
+    // We can use s1 now throught the programme or function
+    println!("Can we use s1 second time: {}", s1);
+}
+
+fn calculate_length_of_string(s: &String) -> (&String, usize) {
+    let length = s.len();
+    (s, length)
 }
