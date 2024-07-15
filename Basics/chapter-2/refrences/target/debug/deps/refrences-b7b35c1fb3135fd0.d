87a8835f688918ccd22149aedd1e046308e03b16
dev-hack95 <saiprasadtoshatwad@gmail.com>
2024-07-15 20:09:37 +0530

    refrences readme update

--- Basics/chapter-2/refrences/target/debug/deps/refrences-b7b35c1fb3135fd0.d
+++
@@ -1,9 +0,0 @@
-/home/ubuntu/Rust/Learn_Rust/Basics/chapter-2/refrences/target/debug/deps/librefrences-b7b35c1fb3135fd0.rmeta: src/main.rs Cargo.toml
-
-/home/ubuntu/Rust/Learn_Rust/Basics/chapter-2/refrences/target/debug/deps/refrences-b7b35c1fb3135fd0.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
